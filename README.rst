2121c929755e62b67952d581a3a0b069402a492f
Colleen Murphy <colleen@gazlene.net>
2016-11-02 17:47:58 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,12 @@
+Google Music Alarm
+==================
+
+Uses the gmusicapi library and vlc python binding to stream music from a Google
+Play Music radio station.
+
+TODO:
+ - Add alarm functionality
+ - Add ability to select station
+ - Use OAuth instead of plaintext credentials
+
+License: Apache 2.0
---
+++ gmusic_alarm/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+import configparser
+
+from gmusicapi import Mobileclient
+import vlc
+
+config = configparser.ConfigParser()
+config.read(os.path.expanduser('~/.gmusic-alarm-creds'))
+username = config.get('credentials', 'username')
+password = config.get('credentials', 'password')
+
+api = Mobileclient()
+
+logged_in = api.login(username, password, Mobileclient.FROM_MAC_ADDRESS)
+if not logged_in:
+    print("Failed to log in.")
+    exit(1)
+device_id = api.get_registered_devices()[0]['id'][2:]
+stations = [station for station in api.get_all_stations() if station['inLibrary']]
+tracks = api.get_station_tracks(stations[0]['id'], num_tracks=1)
+stream_url = api.get_stream_url(tracks[0]['nid'], device_id=device_id)
+print(stream_url)
+vlc_client = vlc.Instance('-v')
+player = vlc_client.media_player_new()
+media = vlc_client.media_new(stream_url)
+player.set_media(media)
+while True:
+    player.play()
+
+print("played a thing")
