1fc6902de5fcd2843bcc94f22b0906102e14b328
Colleen Murphy <colleen@gazlene.net>
2016-11-05 12:31:36 +0100

    Refactor

--- gmusic_alarm/__init__.py
+++ gmusic_alarm/__init__.py
@@ -1,48 +1,11 @@
-import os
-import json
 import time
 
-import configparser
-from contextlib import redirect_stderr
-
-from gmusicapi import Mobileclient
 import vlc
 
-CREDENTIALS_FILE = '~/.gmusic-alarm-creds'
+import gmusic_client
 
-def get_credentials():
-    config = configparser.ConfigParser()
-    config.read(os.path.expanduser(CREDENTIALS_FILE))
-    credentials = {}
-    credentials['username'] = config.get('credentials', 'username')
-    credentials['password'] = config.get('credentials', 'password')
-    return credentials
-
-def login(gclient):
-    credentials = get_credentials()
-    logged_in = gclient.login(credentials['username'],
-                              credentials['password'],
-                              Mobileclient.FROM_MAC_ADDRESS)
-    if not logged_in:
-        print("Failed to log in.")
-        exit(1)
-
-def get_device_id(gclient):
-    # Get all devices, pick the first one, get its ID, strip the leading '0x'
-    device_id = gclient.get_registered_devices()[0]['id'][2:]
-    return device_id
-
-def get_station(gclient):
-    stations = [station for station in gclient.get_all_stations()
-                if station['inLibrary']]
-    return stations[0]
-
-def get_tracks(gclient, stations):
-    tracks = gclient.get_station_tracks(stations['id'])
-    return tracks
 
 def play_tracks(gclient, tracks):
-    device_id = get_device_id(gclient)
     vlc_client = vlc.Instance('--file-caching 3000')
     player = vlc_client.media_player_new()
     # The MediaList type doesn't understand streams properly, so we need to
@@ -50,8 +13,7 @@ def play_tracks(gclient, tracks):
     # player and start the next song using just the Media type.
     for track in tracks:
         duration = int(track['durationMillis'])
-        stream_url = gclient.get_stream_url(track['nid'],
-                                            device_id=device_id)
+        stream_url = gclient.get_stream_url(track['nid'])
         media = vlc_client.media_new(stream_url)
         player.set_media(media)
         # +3000 - Give laggy songs extra time to finish
@@ -60,10 +22,9 @@ def play_tracks(gclient, tracks):
             player.play()
 
 def main():
-    gclient = Mobileclient()
-    login(gclient)
-    station = get_station(gclient)
-    tracks = get_tracks(gclient, station)
+    gclient = gmusic_client.GClient()
+    gclient.login()
+    tracks = gclient.get_tracks()
     play_tracks(gclient, tracks)
 
 if __name__ == '__main__':
---
+++ gmusic_alarm/gmusic_client.py
@@ -0,0 +1,48 @@
+import os
+import configparser
+
+from gmusicapi import Mobileclient
+
+CREDENTIALS_FILE = '~/.gmusic-alarm-creds'
+
+class GClient:
+
+    def __init__(self):
+        self._gclient = Mobileclient()
+        self._credentials = self._get_credentials()
+
+    def _get_credentials(self):
+        config = configparser.ConfigParser()
+        config.read(os.path.expanduser(CREDENTIALS_FILE))
+        credentials = {}
+        credentials['username'] = config.get('credentials', 'username')
+        credentials['password'] = config.get('credentials', 'password')
+        return credentials
+
+    def login(self):
+        logged_in = self._gclient.login(self._credentials['username'],
+                                        self._credentials['password'],
+                                        Mobileclient.FROM_MAC_ADDRESS)
+        if not logged_in:
+            print("Failed to log in.")
+            exit(1)
+
+
+    def get_device_id(self):
+        # Get all devices, pick the first one, get its ID, strip the leading '0x'
+        self.device_id = self._gclient.get_registered_devices()[0]['id'][2:]
+        return self.device_id
+
+    def get_station(self):
+        stations = [station for station in self._gclient.get_all_stations()
+                    if station['inLibrary']]
+        return stations[0]
+
+    def get_tracks(self):
+        station = self.get_station()
+        tracks = self._gclient.get_station_tracks(station['id'])
+        return tracks
+
+    def get_stream_url(self, track_id):
+        device_id = self.get_device_id()
+        return self._gclient.get_stream_url(track_id, device_id)
